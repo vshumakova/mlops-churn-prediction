9e4ac8e8c7c07ac5c0dbca8868d4e95c016ba5ee
Valentina Shumakova <vshumakova01@gmail.com>
2026-06-06 17:06:51 +0300

    Update train.py

--- src/train.py
+++ src/train.py
@@ -45,13 +45,6 @@ metrics = {
 }
 print(f"Metrics: {metrics}")
 
-# with mlflow.start_run():
-#     mlflow.log_params(model.get_params())
-#     mlflow.log_metrics(metrics)
-#     mlflow.sklearn.log_model(model, "churn_model")
-#     mlflow.log_param("features", available_features)
-#     print("\nLogged to MLflow")
-
 # Saving model
 joblib.dump(model, 'models/model.pkl')
 print("Model saved")
